a5b5fcfa07cef8a46ac438a1d5a4076b7c631375
rouzbeh <rouzbeh@videoplaza.com>
2011-05-30 16:06:35 +0200

    vp rawlog specific transform rules

--- src/main/scala/transform.scala
+++ src/main/scala/transform.scala
@@ -18,6 +18,35 @@ object tapp extends Application {
 
 	val sdf = new SimpleDateFormat("yyyy-MM-dd HH:00")
 	
+	
+	// selector functions
+	val f_name: Emit.Selector = _ map {
+		case "1" => "midroll"
+		case "2" => "overlay"
+		case "3" => "preroll"
+		case "4" => "postroll"
+		case "5" => "companion_banner"
+		case "6" => "inskin"
+		case "7" => "splash"
+		case _   => "unknown"
+	}
+
+	val e_name: Emit.Selector = _ map {
+		case  "0" => "impression"
+		case "20" => "click_through"
+		case "90" => "companion_impression"
+		case "92" => "companion_click_through"
+		case "13" => "clip_start"
+		case "14" => "ad_start"
+		case "15" => "ad_first_quartile"
+		case "16" => "ad_midpoint"
+		case "17" => "ad_third_quartile"
+		case "19" => "ad_complete"
+		case "21" => "slot_start"
+		case "22" => "slot_end"
+		case _   => "unknown"
+	}
+
 	val hour: Emit.Selector = _ map {
 		case Long(ms) => sdf.format(ms)
 		case _ => error("not a number")
@@ -26,22 +55,25 @@ object tapp extends Application {
 	def splitcat(d:String): List[String] => List[String] = { case x:List[String] => cat(d)(split(x)) }
 	
 	// parameter definitions
-	val $a = param("a")
-	val $b = param("b")
+	val $site = param("sid")
+	val $ad = param("a")
+	val $event = param("e")
+	val $format = param("t")
+	val $tags = param("tags")
+	val $rs = param("rs")
+	val $rsa = param("rsa")
+	val $sh = param("shares")
+	val $ts = param("ts")
 
 	override def main(args: Array[String]): Unit = {
-		if (args.length < 1) {
-			println("run with:")
-			println("  <input>")
-			return
-		}
-
 		// transform rule definition
 		val loggy = transform.rule
-		expand ~ $a + "," + $b{splitcat("/")} in loggy
+		expand ~ $event{e_name} + "," + $ts{hour} + "," + $ad + "," + $format{f_name} + "," + $rsa{splitcat("/")} + "," + $tags{splitcat("/")}  in loggy
+
+		println("event,time,ad,format,shares,tags")
 
 		val app = new App(loggy)
-		for( line <- scala.io.Source.fromFile(args(1)).getLines ) {
+		for( line <- io.Source.stdin.getLines ) {
 		    app process line
 		}
 	}
